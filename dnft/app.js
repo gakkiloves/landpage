f9cc1ef97ba5d7b71df0250285d40a52c0c78ac5
gakkiloves <mulander2011@outlook.com>
2021-01-31 16:58:03 +0800

    dnft:update

---
+++ dnft/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+const compression = require('compression')
+
+const app = express()
+
+//  开启gzip压缩
+app.use(compression())
+//  配置静态资源路径
+app.use(express.static(path.join(__dirname, 'dist')))
+
+//  定义端口
+const port = 8001
+//  启动网络服务监听
+app.listen(port, () => {
+    console.log(`App listening at port ${port}`)
+})
