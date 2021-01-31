7ec8eef58f9ed19c62726f86b3c21ffe6c401c2c
gakkiloves <mulander2011@outlook.com>
2021-01-31 18:21:33 +0800

    dnft:update

--- dnft/vue.config.js
+++ dnft/vue.config.js
@@ -29,7 +29,17 @@ module.exports = {
                 outputPath: './vendor'
             })
         )
+
     },
+    // chainWebpack(config){
+    //     // 图片压缩
+    //     config.module
+    //         .rule('images')
+    //         .use('image-webpack-loader')
+    //         .loader('image-webpack-loader')
+    //         .options({ bypassOnDebug: true })
+    //         .end()
+    // },
     pluginOptions: {
         quasar: {
             importStrategy: 'kebab',
