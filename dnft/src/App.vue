e8d08cabf804393a265c9e3b851e5e485672b0ff
Mulander-J <mulander_j@outlook.com>
2021-03-16 13:53:04 +0800

    feat:update dnft

--- dnft/src/App.vue
+++ dnft/src/App.vue
@@ -358,10 +358,10 @@
                     <q-space></q-space>
                     <div class="row q-gutter-x-md">
                         <q-btn type="a" href="https://github.com/DNFT-Team/" target="_blank" flat round dense color="grey" size="sm" icon="bt-github"></q-btn>
-                        <q-btn type="a" href="https://twitter.com/_DNFT_" target="_blank"  flat round dense color="grey" size="sm" icon="bt-twitter"></q-btn>
-<!--                        <q-btn flat round dense color="grey" size="sm" icon="bt-telegram"></q-btn>-->
-<!--                        <q-btn flat round dense color="grey" size="sm" icon="bt-linkedin"></q-btn>-->
-<!--                        <q-btn flat round dense color="grey" size="sm" icon="bt-facebook"></q-btn>-->
+                        <q-btn type="a" href="https://https://t.me/dnftprotocol" target="_blank"  flat round dense color="grey" size="sm" icon="bt-telegram"></q-btn>
+                        <q-btn type="a" href="https://discord.gg/avG3rsebEX" target="_blank"  flat round dense color="grey" size="sm" icon="bt-discord"></q-btn>
+                        <q-btn type="a" href="https://twitter.com/DNFTfomo" target="_blank"  flat round dense color="grey" size="sm" icon="bt-twitter"></q-btn>
+                        <q-btn type="a" href="https://medium.com/dnft-protocol" target="_blank"  flat round dense color="grey" size="sm" icon="bt-medium"></q-btn>
                     </div>
                 </q-toolbar>
                 <p class="text-center dnft-rights">Copyright © 2021 DNFT</p>
